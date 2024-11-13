38e0cfabd0c1ed274e3db1784c9bb49530ed1e68
nguyenletutaibmek20 <nguynltt@gmail.com>
2024-11-13 03:35:01 -0500

    tai

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,124 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include <TFT_eSPI.h>
 #include <lvgl.h>
-#include "TAMC_GT911.h"
-/*Set to your screen resolution and rotation*/
-#define TFT_HOR_RES 320
-#define TFT_VER_RES 480
-#define TFT_ROTATION LV_DISPLAY_ROTATION_90
+#include <TFT_eSPI.h>
+#include <ui.h>
+#include <TAMC_GT911.h>  // Include GT911 touch library
 
-/*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
-#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
-uint32_t draw_buf[DRAW_BUF_SIZE / 4];
+/* Screen resolution */
+static const uint16_t screenWidth = 480;
+static const uint16_t screenHeight = 320;
 
-TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
+/* Buffer size for partial rendering (1/10 of screen size) */
+enum { SCREENBUFFER_SIZE_PIXELS = screenWidth * screenHeight / 20 };
+static lv_color_t buf[SCREENBUFFER_SIZE_PIXELS]; 
 
-TAMC_GT911 tp = TAMC_GT911(TOUCH_SDA, TOUCH_SCL, TOUCH_INT, TOUCH_RST, TFT_WIDTH, TFT_HEIGHT);
-/*Read the touchpad*/
-void my_touchpad_read(lv_indev_t *indev, lv_indev_data_t *data)
+TFT_eSPI tft = TFT_eSPI();  // TFT instance
+
+/* GT911 Touch Controller Initialization */
+TAMC_GT911 tp = TAMC_GT911(TOUCH_SDA, TOUCH_SCL, TOUCH_INT, TOUCH_RST, screenWidth, screenHeight);
+
+#if LV_USE_LOG != 0
+/* Serial debugging */
+void my_print(const char * buf)
 {
-  tp.read();
-  if (tp.isTouched)
-  {
-    for (int i = 0; i < tp.touches; i++)
+    Serial.printf(buf);
+    Serial.flush();
+}
+#endif
+
+/* Display flushing */
+void my_disp_flush (lv_display_t *disp, const lv_area_t *area, uint8_t *pixelmap)
+{
+    uint32_t w = (area->x2 - area->x1 + 1);
+    uint32_t h = (area->y2 - area->y1 + 1);
+
+    #ifdef LV_COLOR_OP_H
+        // Assuming LV_COLOR_OP_H should be evaluated as true if defined
+        size_t len = lv_area_get_size(area);
+        lv_draw_sw_rgb565_swap(pixelmap, len);
+    #else
+        printf("LV_COLOR_OP_H is not defined, skipping color operation.\n");
+    #endif
+
+    tft.startWrite();
+    tft.setAddrWindow(area->x1, area->y1, qqw, h);
+    tft.pushColors((uint16_t*)pixelmap, w * h, true);
+    tft.endWrite();
+
+    lv_disp_flush_ready(disp);
+}
+
+/* Read the touchpad */
+void my_touchpad_read(lv_indev_t *indev_driver, lv_indev_data_t *data)
+{
+    tp.read();  // Read touch points from GT911 controller
+    if (tp.isTouched)
     {
-      data->state = LV_INDEV_STATE_PRESSED;
-      data->point.x = tp.points[i].x;
-      data->point.y = tp.points[i].y;
+        for (int i = 0; i < tp.touches; i++)
+        {
+            data->state = LV_INDEV_STATE_PRESSED;
+            // Adjust touch coordinates to match landscape orientation
+            data->point.x = tp.points[i].y;               // Swap x and y
+            data->point.y = screenHeight - tp.points[i].x; // Invert y for landscape
+        }
     }
-  }
-  else
-  {
-    data->state = LV_INDEV_STATE_RELEASED;
-  }
-}
-/*use Arduinos millis() as tick source*/
-static uint32_t my_tick(void)
-{
-  return millis();
-}
-static void event_cb2(lv_event_t * e)
-{
-    /*The original target of the event. Can be the buttons or the container*/
-    lv_obj_t * target = (lv_obj_t*)lv_event_get_target(e);
-
-    /*The current target is always the container as the event is added to it*/
-    lv_obj_t * cont = (lv_obj_t*)lv_event_get_current_target(e);
-
-    /*If container was clicked do nothing*/
-    if(target == cont) return;
-
-    /*Make the clicked buttons red*/
-    lv_obj_set_style_bg_color(target, lv_palette_main(LV_PALETTE_RED), 0);
-}
-void setup()
-{
-  pinMode(TFT_BL, OUTPUT);
-  digitalWrite(TFT_BL, HIGH); // Set the Back-light on
-  String LVGL_Arduino = "Hello Arduino! ";
-  LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
-  Serial.begin(115200);
-  Serial.println(LVGL_Arduino);
-  lv_init();
-  lv_tick_set_cb(my_tick);
-  lv_display_t *disp;
-  /*TFT_eSPI can be enabled lv_conf.h to initialize the display in a simple way*/
-  disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
-  //.pio\libdeps\m5stack-stamps3\lvgl\src\drivers\display\tft_espi\lv_tft_espi.cpp
-  // change rotation to 4
-  // dsc->tft->setRotation(4);   /* Landscape orientation, flipped */
-  tft.invertDisplay(true); // Where i is true or false
-  tft.fillScreen(TFT_BLACK);
-  lv_display_set_rotation(disp, TFT_ROTATION);
-
-  tp.begin();
-  tp.setRotation(ROTATION_NORMAL);
-  /*Initialize the (dummy) input device driver*/
-  lv_indev_t *indev = lv_indev_create();
-  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
-  lv_indev_set_read_cb(indev, my_touchpad_read);
-  lv_obj_t *label = lv_label_create(lv_screen_active());
-  lv_label_set_text(label, "Hello Arduino, I'm LVGL!");
-  lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
-  delay(1000);
-  // example 3
-  lv_obj_t *cont = lv_obj_create(lv_screen_active());
-  lv_obj_set_size(cont, 480, 320);
-  lv_obj_center(cont);
-  lv_obj_set_flex_flow(cont, LV_FLEX_FLOW_ROW_WRAP);
-
-  uint32_t i;
-  for (i = 0; i < 30; i++)
-  {
-    lv_obj_t *btn = lv_button_create(cont);
-    lv_obj_set_size(btn, 70, 50);
-    lv_obj_add_flag(btn, LV_OBJ_FLAG_EVENT_BUBBLE);
-
-    lv_obj_t *label = lv_label_create(btn);
-    lv_label_set_text_fmt(label, "%" LV_PRIu32, i);
-    lv_obj_center(label);
-  }
-
-  lv_obj_add_event_cb(cont, event_cb2, LV_EVENT_CLICKED, NULL);
+    else
+    {
+        data->state = LV_INDEV_STATE_RELEASED;
+    }
 }
 
-void loop()
+/* Set tick routine needed for LVGL internal timings */
+static uint32_t my_tick_get_cb (void) { return millis(); }
+
+void setup ()
 {
-  lv_task_handler();
-  delay(5);
+    Serial.begin(115200); /* Prepare for possible serial debug */
+
+    String LVGL_Arduino = "Hello Arduino! ";
+    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+
+    Serial.println(LVGL_Arduino);
+    Serial.println("I am LVGL_Arduino");
+
+    lv_init();
+
+#if LV_USE_LOG != 0
+    lv_log_register_print_cb(my_print); /* Register print function for debugging */
+#endif
+
+    pinMode(TFT_BL, OUTPUT);
+    digitalWrite(TFT_BL, HIGH); // Set the backlight on
+    tft.init();                 // Initialize TFT
+    tft.setRotation(1);         // Set rotation to 1 for landscape mode
+    tft.fillScreen(TFT_BLACK);
+
+    /* Initialize display buffer and driver */
+    static lv_disp_t* disp;
+    disp = lv_display_create(screenWidth, screenHeight);
+    lv_display_set_buffers(disp, buf, NULL, SCREENBUFFER_SIZE_PIXELS * sizeof(lv_color_t), LV_DISPLAY_RENDER_MODE_PARTIAL);
+    lv_display_set_flush_cb(disp, my_disp_flush);
+
+    /* Initialize touch controller */
+    tp.begin();
+    tp.setRotation(ROTATION_NORMAL); // Set touch rotation for landscape
+
+    /* Initialize input device driver */
+    static lv_indev_t* indev;
+    indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /* Touchpad should have POINTER type */
+    lv_indev_set_read_cb(indev, my_touchpad_read);
+
+    /* Set the tick source for LVGL */
+    lv_tick_set_cb(my_tick_get_cb);
+
+    /* Initialize UI */
+    ui_init();
+
+    Serial.println("Setup done");
+}
+
+void loop ()
+{
+    lv_timer_handler(); /* Let the GUI do its work */
+    delay(5);
 }
